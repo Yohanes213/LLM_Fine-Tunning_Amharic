fb4e3e2128ec8741254d8b83e7492962ad248bca
GetachewAbebeA <gechachin@gmail.com>
2024-06-01 14:20:05 +0000

    Practicing on Finetuning llama2

---
+++ front_end/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ChatBox from "./components/chatbox";
+const App = () =>{
+  return (
+    <div className="App">
+         <ChatBox/>
+    </div>
+  );
+}
+
+export default App;
